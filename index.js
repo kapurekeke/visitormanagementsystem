a39d99b652ce503153c33a904da74adb804bf7cd
hazim <hazimfahmi@gmail.com>
2023-12-23 13:10:32 +0800

    index

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-//test
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000;
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+   res.send('Hello World!')
+})
+
+app.listen(port, () => {
+   console.log(`Example app listening on port ${port}`)
+})
