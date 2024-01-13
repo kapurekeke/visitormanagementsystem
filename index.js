d72686789871796d9d0ab12d1ecd5b4b15fb5825
hazim <hazimfahmi@gmail.com>
2024-01-14 05:22:51 +0800

    update

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const express = require('express')
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const app = express()
-const port = process.env.PORT || 3000;
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const swaggerui = require('swagger-ui-express');
 const jwt = require('jsonwebtoken');
 const swaggerjsdoc = require('swagger-jsdoc');
 
-app.use(express.json())
+const app = express();
+const port = process.env.PORT || 3000;
 
 // MongoDB connection URL
 const uri = "mongodb+srv://hajimu69:hAZimFAhm1kaYKaY24@cluster1.gljgb6e.mongodb.net/";
 
 // Create a new MongoClient
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true }, {serverApi:
-    {version: ServerApiVersion.v1,
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true }, {
+  serverApi: {
+    version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-}
+  },
 });
 
 // Connect to MongoDB
@@ -30,49 +30,82 @@ client.connect()
 
 // Define collection names
 const db = client.db('vms');
-const adminCollection = db.collection ('admin');
-const visitorCollection = db.collection ('visitor');
+const adminCollection = db.collection('admin');
+const visitorCollection = db.collection('visitor');
 const prisonerCollection = db.collection('prisoner');
-const visitorPassCollection = db.collection('visitorpass');
-const visitorcredentialsCollection = db.collection('visitorcredentials');
 
-/**login admin function*/
-async function login(reqUsername, reqPassword) {
-    return adminCollection.findOne({ username: reqUsername, password: reqPassword })
-      .then(matchUsers => {
-        if (!matchUsers) {
-          return {
-            success: false,
-            message: "Admin not found!"
-          };
-        } else {
-          return {
-            success: true,
-            users: matchUsers
-          };
-        }
-      })
-      .catch(error => {
-          console.error('Error in login:', error);
-          return {
-            success: false,
-            message: "An error occurred during login."
-          };
-        });
+// Middleware to parse JSON
+app.use(express.json());
+
+// Authentication middleware
+function verifyToken(req, res, next) {
+  let header = req.headers.authorization;
+
+  // Check if the Authorization header is present
+  if (!header) {
+    return res.status(401).send('Authorization header is missing');
+  }
+
+  // Split the header to extract the token
+  let tokenParts = header.split(' ');
+
+  // Check if the expected token format is present
+  if (tokenParts.length !== 2 || tokenParts[0].toLowerCase() !== 'bearer') {
+    return res.status(401).send('Invalid Authorization header format');
+  }
+
+  let token = tokenParts[1];
+
+  jwt.verify(token, 'inipassword', function (err, decoded) {
+    if (err) {
+      return res.status(401).send('Invalid Token');
+    }
+
+    req.user = decoded;
+    next();
+  });
 }
 
-// Visitor pass function
+// Function to generate JWT token
+function generateToken(userData) {
+  const token = jwt.sign(userData, 'inipassword');
+  return token;
+}
+
+// Function to handle admin login
+async function login(reqUsername, reqPassword) {
+  try {
+    const matchUsers = await adminCollection.findOne({ username: reqUsername, password: reqPassword });
+
+    if (!matchUsers) {
+      return {
+        success: false,
+        message: "Admin not found!",
+      };
+    } else {
+      return {
+        success: true,
+        users: matchUsers,
+      };
+    }
+  } catch (error) {
+    console.error('Error in login:', error);
+    return {
+      success: false,
+      message: "An error occurred during login.",
+    };
+  }
+}
+
+// Function to check visitor pass
 async function visitorspass(reqicnum) {
   try {
-    const matchedUser = await visitorPassCollection.findOne({ icNum: reqicnum });
+    const matchedUser = await visitorCollection.findOne({ icnumber: reqicnum });
 
     if (matchedUser) {
       return {
         success: true,
-        message: {
-          status: matchedUser.status || 'pending',
-          details: matchedUser,
-        },
+        message: matchedUser,
       };
     } else {
       return {
@@ -89,249 +122,30 @@ async function visitorspass(reqicnum) {
   }
 }
 
-// Visitor pass request function
-async function requestVisitorPass(reqIcNum, reqDate, reqTime, reqPrisonerId) {
-  const requestDetails = {
-    icNum: reqIcNum,
-    date: reqDate,
-    time: reqTime,
-    prisonerId: reqPrisonerId,
-    status: 'pending', // Initial status is set to pending
-    approvedByAdmin: null, // Admin approval information
-  };
-
-  try {
-    const result = await visitorPassCollection.insertOne(requestDetails);
-    return {
-      success: true,
-      message: 'Visitor pass request submitted successfully!',
-      requestId: result.insertedId,
-    };
-  } catch (error) {
-    console.error('Error submitting visitor pass request:', error);
-    return {
-      success: false,
-      message: 'An error occurred while submitting the visitor pass request.',
-    };
-  }
-}
-
-async function approveVisitorPass(requestId, approvalStatus) {
-  try {
-    const updatedRequest = await visitorPassCollection.findOneAndUpdate(
-      { _id: ObjectId(requestId) },
-      { $set: { status: approvalStatus, approvedByAdmin: req.user.username } },
-      { returnDocument: 'after' }
-    );
-
-    if (updatedRequest.value) {
-      return {
-        success: true,
-        message: `Visitor pass request ${approvalStatus === 'approved' ? 'approved' : 'declined'} successfully by admin.`,
-      };
-    } else {
-      return {
-        success: false,
-        message: 'Visitor pass request not found.',
-      };
-    }
-  } catch (error) {
-    console.error('Error approving/declining visitor pass request:', error);
-    return {
-      success: false,
-      message: 'Internal server error',
-    };
-  }
-}
-
-//visitor login function
-async function visitorLogin(reqUsername, reqPassword) {
-  try {
-    const matchedUsers = await visitorcredentialsCollection.findOne({ username: reqUsername, password: reqPassword });
-
-    if (!matchedUsers) {
-      return {
-        success: false,
-        message: "Visitor not found!",
-      };
-    } else {
-      return {
-        success: true,
-        users: matchedUsers,
-      };
-    }
-  } catch (error) {
-    console.error('Error in visitor login:', error);
-    return {
-      success: false,
-      message: "An error occurred during visitor login.",
-    };
-  }
-}
-
-
-
-/**create admin function */
+// Function to register admin
 async function register(reqUsername, reqPassword) {
-    return adminCollection.insertOne({
-      username: reqUsername,
-      password: reqPassword,
-      
-    })
-      .then(() => {
-        return "Registration successful!";
-      })
-      .catch(error => {
-        console.error('Registration failed:', error);
-        return "Error encountered!";
-      });
-}
-
-//create visitor function
-async function visitorRegister(reqFirstName, reqLastName, reqPhoneNum, reqUsername, reqPassword) {
   try {
-    await visitorcredentialsCollection.insertOne({
-      firstName: reqFirstName,
-      lastName: reqLastName,
-      phoneNum: reqPhoneNum,
+    await adminCollection.insertOne({
       username: reqUsername,
       password: reqPassword,
     });
 
-    // Return the username or user information after successful registration
-    return reqUsername;
+    return "Registration successful!";
   } catch (error) {
-    console.error('Visitor registration failed:', error);
-    throw new Error("Error encountered during visitor registration!");
+    console.error('Registration failed:', error);
+    return "Error encountered!";
   }
 }
 
-
-function generateAdminToken(userData) {
-  const token = jwt.sign(userData, 'adminSecretKey');
-  return token;
-}
-
-function generateVisitorToken(userData) {
-  const token = jwt.sign(userData, 'visitorSecretKey');
-  return token;
-}
-
-function verifyAdminToken(req, res, next) {
-  let header = req.headers.authorization;
-
-  // Check if the Authorization header is present
-  if (!header) {
-    return res.status(401).send('Authorization header is missing');
-  }
-
-  console.log(header);
-
-  // Split the header to extract the token
-  let tokenParts = header.split(' ');
-
-  // Check if the expected token format is present
-  if (tokenParts.length !== 2 || tokenParts[0].toLowerCase() !== 'bearer') {
-    return res.status(401).send('Invalid Authorization header format');
-  }
-
-  let token = tokenParts[1];
-
-  jwt.verify(token, 'adminSecretKey', function (err, decoded) {
-    if (err) {
-      return res.status(401).send('Invalid Token');
-    }
-
-    req.user = decoded;
-    next();
-  });
-}
-
-function verifyVisitorToken(req, res, next) {
-  let header = req.headers.authorization;
-
-  // Check if the Authorization header is present
-  if (!header) {
-    return res.status(401).send('Authorization header is missing');
-  }
-
-  console.log(header);
-
-  // Split the header to extract the token
-  let tokenParts = header.split(' ');
-
-  // Check if the expected token format is present
-  if (tokenParts.length !== 2 || tokenParts[0].toLowerCase() !== 'bearer') {
-    return res.status(401).send('Invalid Authorization header format');
-  }
-
-  let token = tokenParts[1];
-
-  jwt.verify(token, 'visitorSecretKey', function (err, decoded) {
-    if (err) {
-      return res.status(401).send('Invalid Token');
-    }
-
-    req.user = decoded;
-    next();
-  });
-}
+// API Routes
 
 // Login Admin
 app.post('/login', (req, res) => {
-    console.log(req.body);
-  
-    let result = login(req.body.username, req.body.password);
-    result.then(response => {
-      console.log(response); // Log the response received
-  
-      if (response.success) {
-        let token = generateAdminToken(response.users);
-        res.send("Admin Auth Token: " + token);
-      } else {
-        res.status(401).send(response.message);
-      }
-    }).catch(error => {
-      console.error('Error in login route:', error);
-      res.status(500).send("An error occurred during login.");
-    });
-});
-
-//visitor register
-app.post('/registervisitor', async (req, res) => {
-  try {
-    console.log(req.body);
-
-    // Register the visitor and get the username
-    const username = await visitorRegister(req.body.firstName, req.body.lastName, req.body.phoneNum, req.body.username, req.body.password);
-
-    // Log in the visitor
-    const loginResult = await visitorLogin(username, req.body.password);
-
-    if (loginResult.success) {
-      const token = generateVisitorToken(loginResult.users);
-      res.send("Visitor Auth Token: " + token);
-    } else {
-      res.status(401).send(loginResult.message);
-    }
-  } catch (error) {
-    console.error('Error in register route:', error);
-    res.status(500).send("An error occurred during registration.");
-  }
-});
-
-
-//visitor login
-app.post('/loginvisitor', async(req, res) => {
-  console.log(req.body);
-
-  let result = visitorLogin(req.body.username, req.body.password);
+  let result = login(req.body.username, req.body.password);
   result.then(response => {
-    console.log(response); // Log the response received
-
     if (response.success) {
-      let token = generateVisitorToken(response.users);
-      res.send("Visitor Auth Token: " + token);
+      let token = generateToken(response.users);
+      res.send("Auth Token: " + token);
     } else {
       res.status(401).send(response.message);
     }
@@ -341,171 +155,101 @@ app.post('/loginvisitor', async(req, res) => {
   });
 });
 
+// Register Admin
+app.post('/register', verifyToken, (req, res) => {
+  let result = register(req.body.username, req.body.password);
+  result.then(response => {
+    res.send(response);
+  }).catch(error => {
+    console.error('Error in register route:', error);
+    res.status(500).send("An error occurred during registration.");
+  });
+});
 
-
-// visitor pass
-app.post('/visitorspass/:icnum', verifyVisitorToken, async (req, res) => {
+//visitors pass
+app.post('/visitorspass/:icnum', async (req, res) => {
   const { icnum } = req.params;
 
   try {
     const result = await visitorspass(icnum);
     res.json(result);
   } catch (error) {
-    res.status(500).json({ success: false, message: 'Internal server error' });
+    res.status(500).json({ success: false, message: "Internal server error" });
   }
-});
-
-// Visitor request visitor pass
-app.post('/visitor/requestpass', verifyVisitorToken, async (req, res) => {
-  const { icNum, date, time, prisonerId } = req.body;
-
-  try {
-    const result = await requestVisitorPass(icNum, date, time, prisonerId);
-    res.json(result);
-  } catch (error) {
-    res.status(500).json({ success: false, message: 'Internal server error' });
-  }
-});
-
-// Admin approve/decline visitor pass request
-app.put('/admin/approvevisitorpass/:requestId', verifyAdminToken, async (req, res) => {
-  const { requestId } = req.params;
-  const { approvalStatus } = req.body;
-
-  try {
-    const updatedRequest = await visitorPassCollection.findOneAndUpdate(
-      { _id: ObjectId(requestId) },
-      { $set: { status: approvalStatus, approvedByAdmin: req.user.username } },
-      { returnDocument: 'after' }
-    );
-
-    if (updatedRequest.value) {
-      res.json({
-        success: true,
-        message: `Visitor pass request ${approvalStatus === 'approved' ? 'approved' : 'declined'} successfully by admin.`,
-      });
-    } else {
-      res.status(404).json({ success: false, message: 'Visitor pass request not found.' });
-    }
-  } catch (error) {
-    console.error('Error approving/declining visitor pass request:', error);
-    res.status(500).json({ success: false, message: 'Internal server error' });
-  }
-});
-
-
-// Register Admin
-app.post('/register', verifyAdminToken, (req, res) => {
-    console.log(req.body);
-  
-    let result = register(req.body.username, req.body.password, req.body.name, req.body.email);
-    result.then(response => {
-      res.send(response);
-    }).catch(error => {
-      console.error('Error in register route:', error);
-      res.status(500).send("An error occurred during registration.");
-    });
 });
 
 
 // Add a visitor
-app.post('/addvisitor', verifyAdminToken, (req, res) => {
-    const {
-      name,
-      icnumber,
-      relationship,
-      prisonerId,
-      date,
-      time
-    } = req.body;
-  
-    const visitorData = {
-      name,
-      icnumber,
-      relationship,
-      prisonerId,
-      date,
-      time
-    };
-  
-    visitorCollection
-      .insertOne(visitorData)
-      .then(() => {
-        res.send(visitorData);
-      })
-      .catch((error) => {
-        console.error('Error adding visitor:', error);
-        res.status(500).send('An error occurred while adding the visitor');
-      });
+app.post('/addvisitor', verifyToken, (req, res) => {
+  const { name, icnumber, relationship, prisonerId, date, time } = req.body;
+  const visitorData = { name, icnumber, relationship, prisonerId, date, time };
+
+  visitorCollection.insertOne(visitorData)
+    .then(() => {
+      res.send(visitorData);
+    })
+    .catch((error) => {
+      console.error('Error adding visitor:', error);
+      res.status(500).send('An error occurred while adding the visitor');
+    });
 });
 
-
 // Add a prisoner
-app.post('/addprisoner', verifyAdminToken, (req, res) => {
-    const {
-      name,
-      icnumber,
-      prisonerId
-    } = req.body;
-  
-    const prisonerData = {
-      name,
-      icnumber,
-      prisonerId
-    };
-  
-    prisonerCollection
-      .insertOne(prisonerData)
-      .then(() => {
-        res.send(prisonerData);
-      })
-      .catch((error) => {
-        console.error('Error creating prisoner:', error);
-        res.status(500).send('An error occurred while creating the prisoner');
-      });
+app.post('/addprisoner', verifyToken, (req, res) => {
+  const { name, icnumber, prisonerId } = req.body;
+  const prisonerData = { name, icnumber, prisonerId };
+
+  prisonerCollection.insertOne(prisonerData)
+    .then(() => {
+      res.send(prisonerData);
+    })
+    .catch((error) => {
+      console.error('Error creating prisoner:', error);
+      res.status(500).send('An error occurred while creating the prisoner');
+    });
 });
 
 // View all visitors
-app.get('/visitors',verifyAdminToken, async (req, res) => {
-    try {
-      const db = client.db('vms');
-      const prisoner = await db.collection('visitor').find().toArray();
-      res.send(prisoner);
-    } catch (error) {
-      res.status(500).send('Error viewing visitors');
-    }
+app.get('/visitors', verifyToken, async (req, res) => {
+  try {
+    const prisoner = await db.collection('visitor').find().toArray();
+    res.send(prisoner);
+  } catch (error) {
+    res.status(500).send('Error viewing visitors');
+  }
 });
 
 // View all prisoner
-app.get('/prisoner', verifyAdminToken, async (req, res) => {
-    try {
-      const db = client.db('vms');
-      const prisoner = await db.collection('prisoner').find().toArray();
-      res.send(prisoner);
-    } catch (error) {
-      res.status(500).send('Error viewing prisoner');
-    }
-  });
+app.get('/prisoner', verifyToken, async (req, res) => {
+  try {
+    const prisoner = await db.collection('prisoner').find().toArray();
+    res.send(prisoner);
+  } catch (error) {
+    res.status(500).send('Error viewing prisoner');
+  }
+});
 
+// Swagger Documentation
 const options = {
-    definition: {
-        openapi: '3.0.0',
-        servers: [
-            {
-              url: 'https://vmsprison.azurewebsites.net',
-            },
-          ],          
-        info: {
-            title: 'VMS PRISON API',
-            version: '1.0.0',
-        },
+  definition: {
+    openapi: '3.0.0',
+    servers: [
+      {
+        url: 'https://vmsprison.azurewebsites.net',
+      },
+    ],
+    info: {
+      title: 'VMS PRISON API',
+      version: '1.0.0',
     },
-    apis: ['./main.js'],
+  },
+  apis: ['./main.js'],
 };
 
 const specs = swaggerjsdoc(options);
 app.use('/api-docs', swaggerui.serve, swaggerui.setup(specs));
 
+// Start the server
 app.listen(port, () => {
-   console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
--- main.js
+++ main.js
@@ -17,8 +17,6 @@
  *     description: Visitor operations
  *   - name: Prisoner
  *     description: Prisoner operations
- *   - name: VisitorPass
- *     description: Visitor pass operations
  * 
  */
 
@@ -72,91 +70,6 @@
  *         description: Registration successful
  *       500:
  *         description: Internal server error
- */
-
-/**
- * @swagger
- * /loginvisitor:
- *   post:
- *     summary: Visitor login endpoint
- *     tags: [Visitor]
- *     description: Logs in a visitor and returns an authentication token if successful.
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               username:
- *                 type: string
- *                 description: The username of the visitor.
- *               password:
- *                 type: string
- *                 description: The password of the visitor.
- *     responses:
- *       200:
- *         description: Successful login. Returns a visitor authentication token.
- *         content:
- *           text/plain:
- *             schema:
- *               type: string
- *               example: "Visitor Auth Token: <token>"
- *       401:
- *         description: Unauthorized. Invalid username or password.
- *         content:
- *           text/plain:
- *             schema:
- *               type: string
- *               example: "Invalid username or password."
- *       500:
- *         description: Internal Server Error. An error occurred during login.
- *         content:
- *           text/plain:
- *             schema:
- *               type: string
- *               example: "An error occurred during login."
- */
-
-/**
- * @swagger
- * /registervisitor:
- *   post:
- *     summary: Register as a visitor.
- *     tags: [Visitor]
- *     parameters:
- *       - in: body
- *         name: RequestBody
- *         description: Visitor registration details.
- *         required: true
- *         schema:
- *           type: object
- *           properties:
- *             firstName:
- *               type: string
- *             lastName:
- *               type: string
- *             phoneNum:
- *               type: string
- *             username:
- *               type: string
- *             password:
- *               type: string
- *     responses:
- *       200:
- *         description: Successful registration.
- *         content:
- *           application/json:
- *             example:
- *               success: true
- *               message: Visitor registration successful!
- *       500:
- *         description: Internal server error.
- *         content:
- *           application/json:
- *             example:
- *               success: false
- *               message: Error encountered during visitor registration!
  */
 
 /**
@@ -270,135 +183,36 @@
  * @swagger
  * /visitorspass/{icnum}:
  *   post:
- *     summary: Get visitor's pass information by IC number
- *     description: Retrieve visitor's pass information using the provided IC number.
- *     tags: [VisitorPass]
+ *     summary: Get Visitor Pass Information
+ *     tags: [Visitor]
  *     parameters:
  *       - in: path
  *         name: icnum
  *         required: true
- *         description: The IC number of the visitor.
  *         schema:
  *           type: string
- *     security:
- *       - BearerAuth: []
+ *         description: The IC number of the visitor pass
  *     responses:
- *       200:
- *         description: Successful operation
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 success:
- *                   type: boolean
- *                   description: Indicates if the operation was successful.
- *                 data:
- *                   type: object
- *                   description: The visitor's pass information.
- *       500:
- *         description: Internal server error
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 success:
- *                   type: boolean
- *                   description: Indicates if the operation was unsuccessful.
- *                 message:
- *                   type: string
- *                   description: Error message.
- */
-
-
-/**
- * @swagger
- * /visitor/requestpass:
- *   post:
- *     summary: Submit a visitor pass request.
- *     tags: [VisitorPass]
- *     security:
- *       - BearerAuth: []
- *     parameters:
- *       - in: body
- *         name: RequestBody
- *         description: Visitor pass request details.
- *         required: true
- *         schema:
- *           type: object
- *           properties:
- *             icNum:
- *               type: string
- *             date:
- *               type: string
- *             time:
- *               type: string
- *             prisonerId:
- *               type: string
- *     responses:
- *       200:
- *         description: Successful request submission.
+ *       '200':
+ *         description: Successful response
  *         content:
  *           application/json:
  *             example:
  *               success: true
- *               message: Visitor pass request submitted successfully!
- *               requestId: 1234567890
- *       500:
- *         description: Internal server error.
+ *               user:
+ *                 # Your user data structure here
+ *       '404':
+ *         description: Visitor pass not found
  *         content:
  *           application/json:
  *             example:
  *               success: false
- *               message: An error occurred while submitting the visitor pass request.
- */
-
-/**
- * @swagger
- * /admin/approvevisitorpass/{requestId}:
- *   put:
- *     summary: Approve or decline a visitor pass request.
- *     tags: [VisitorPass]
- *     security:
- *       - BearerAuth: []
- *     parameters:
- *       - in: path
- *         name: requestId
- *         description: ID of the visitor pass request.
- *         required: true
- *         schema:
- *           type: string
- *       - in: body
- *         name: RequestBody
- *         description: Admin approval details.
- *         required: true
- *         schema:
- *           type: object
- *           properties:
- *             approvalStatus:
- *               type: string
- *               enum: ['approved', 'declined']
- *     responses:
- *       200:
- *         description: Approval/decline successful.
- *         content:
- *           application/json:
- *             example:
- *               success: true
- *               message: Visitor pass request approved/declined successfully by admin.
- *       404:
- *         description: Visitor pass request not found.
+ *               message: Visitor pass not found!
+ *       '500':
+ *         description: Internal Server Error
  *         content:
  *           application/json:
  *             example:
  *               success: false
- *               message: Visitor pass request not found.
- *       500:
- *         description: Internal server error.
- *         content:
- *           application/json:
- *             example:
- *               success: false
- *               message: Internal server error.
+ *               message: An error occurred.
  */
